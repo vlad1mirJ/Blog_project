b6d16f80b3ccfda0af06561956bb04d544564261
Vladimir Jankauskas <vladimirjankauskas@gmail.com>
2022-04-06 01:07:10 +0300

    Design Changes

--- components/Author.js
+++ components/Author.js
@@ -1,21 +1,21 @@
-import React from 'react'
-import Image from 'next/image'
+import React from "react"
+import Image from "next/image"
 
-function Author({author}) {
+function Author({ author }) {
   return (
-    <div className='text-center mt-20 mb-8 p-20 relative rounded-lg bg-black bg-opacity-20'>
-        <div className='absolute left-0 right-0 -top-14'>
-          <Image
-            unoptimized 
-            alt={author.name}
-            height='100px'
-            width='100px'
-            className='align-middle rounded-full'
-            src={author.photo.url}
-          />
-        </div>
-        <h3 className='text-white my-4 text-xl font-bold'>{author.name}</h3>
-        <p className='text-white text-lg'>{author.bio}</p>
+    <div className="text-center mt-20 mb-8 p-20 relative rounded-lg shadow-lg bg-gray-100">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          alt={author.name}
+          height="100px"
+          width="100px"
+          className="align-middle rounded-full"
+          src={author.photo.url}
+        />
+      </div>
+      <h3 className="text-black my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-black text-lg">{author.bio}</p>
     </div>
   )
 }
